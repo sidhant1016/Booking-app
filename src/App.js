ae36f80421805a7136a76e57418559bd30c3d186
sidhant <gagranirocksidhanth@gmail.com>
2023-04-24 15:23:16 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "../src/Pages/home/Home"
+import List from "../src/Pages/list/List"
+import Hotel from "../src/Pages/hotels/Hotels"
+import Flight from "./Pages/flight/Flight.jsx"
+import CarRedntals from "./Pages/carRentals/CarRentals.jsx"
+import Taxies from "./Pages/taxies/Taxies.jsx"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <BrowserRouter>
+   <Routes>
+   <Route path="/" element = {<Home/>}/>
+   <Route path="/hotels" element = {<List/>}/>
+   <Route path="/hotels/:id" element = {<Hotel/>}/>
+   <Route path="/flight" element = {<Flight/>}/>
+   <Route path="/carRentals" element = {<CarRedntals/>}/>
+   <Route path="/taxies" element = {<Taxies/>}/>
+  
+
+   </Routes>
+   
+  
+   </BrowserRouter>
+   
+
   );
 }
 
